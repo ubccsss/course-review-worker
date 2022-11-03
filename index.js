c041720d23b07d944ef169e8881ff3723b6ccbb5
AnimeAllstar <asadrehandhorajiwala@gmail.com>
2022-11-02 18:28:27 -0700

    Re-indent code with Tabs

--- index.js
+++ index.js
@@ -3,27 +3,27 @@ import { Octokit } from '@octokit/rest'
 const octokit = new Octokit({ auth: ACCESS_TOKEN })
 
 const corsHeaders = {
-  'Access-Control-Allow-Origin': ORIGIN,
-  'Access-Control-Allow-Methods': 'POST, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type',
+	'Access-Control-Allow-Origin': ORIGIN,
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
 }
 
 addEventListener('fetch', event => {
-  event.respondWith(handle(event.request))
+	event.respondWith(handle(event.request))
 })
 
 // cors handling based on: https://developers.cloudflare.com/workers/examples/cors-header-proxy
 async function handle(request) {
-  if (request.method === 'OPTIONS') {
-    return handleOptions(request)
-  } else if (request.method === 'POST') {
-    return handleRequest(request)
-  } else {
-    return new Response(null, {
-      status: 405,
-      statusText: 'Method Not Allowed',
-    })
-  }
+	if (request.method === 'OPTIONS') {
+		return handleOptions(request)
+	} else if (request.method === 'POST') {
+		return handleRequest(request)
+	} else {
+		return new Response(null, {
+			status: 405,
+			statusText: 'Method Not Allowed',
+		})
+	}
 }
 
 /**
@@ -31,25 +31,25 @@ async function handle(request) {
  * @param {Request} request
  */
 function handleOptions(request) {
-  const { headers } = request
+	const { headers } = request
 
-  if (
-    headers.get('Origin') !== null &&
-    headers.get('Access-Control-Request-Method') !== null &&
-    headers.get('Access-Control-Request-Headers') !== null
-  ) {
-    // Handle CORS pre-flight request.
-    return new Response(null, {
-      headers: corsHeaders,
-    })
-  } else {
-    // Handle standard OPTIONS request.
-    return new Response(null, {
-      headers: {
-        Allow: 'POST, OPTIONS',
-      },
-    })
-  }
+	if (
+		headers.get('Origin') !== null &&
+		headers.get('Access-Control-Request-Method') !== null &&
+		headers.get('Access-Control-Request-Headers') !== null
+	) {
+		// Handle CORS pre-flight request.
+		return new Response(null, {
+			headers: corsHeaders,
+		})
+	} else {
+		// Handle standard OPTIONS request.
+		return new Response(null, {
+			headers: {
+				Allow: 'POST, OPTIONS',
+			},
+		})
+	}
 }
 
 /**
@@ -57,107 +57,107 @@ function handleOptions(request) {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  if (request.headers.get('Content-Type') !== 'application/json') {
-    return new Response(null, {
-      status: 415,
-      statusText: 'Unsupported Media Type',
-      headers: corsHeaders,
-    })
-  }
+	if (request.headers.get('Content-Type') !== 'application/json') {
+		return new Response(null, {
+			status: 415,
+			statusText: 'Unsupported Media Type',
+			headers: corsHeaders,
+		})
+	}
 
-  // Detect parse failures by setting `json` to null.
-  const json = await request.json().catch(e => null)
+	// Detect parse failures by setting `json` to null.
+	const json = await request.json().catch(e => null)
 
-  if (json === null) {
-    return new Response('JSON parse failure', {
-      status: 400,
-      statusText: 'Bad Request',
-      headers: corsHeaders,
-    })
-  }
+	if (json === null) {
+		return new Response('JSON parse failure', {
+			status: 400,
+			statusText: 'Bad Request',
+			headers: corsHeaders,
+		})
+	}
 
-  // verify reCAPTCHA token
-  const { success, errors } = await verifyToken(json.recaptcha.token)
+	// verify reCAPTCHA token
+	const { success, errors } = await verifyToken(json.recaptcha.token)
 
-  // return error response if reCAPTCHA token is invalid
-  if (!success) {
-    return new Response(JSON.stringify({ errors }), {
-      status: 400,
-      statusText: 'Bad Request',
-      headers: corsHeaders,
-    })
-  }
+	// return error response if reCAPTCHA token is invalid
+	if (!success) {
+		return new Response(JSON.stringify({ errors }), {
+			status: 400,
+			statusText: 'Bad Request',
+			headers: corsHeaders,
+		})
+	}
 
-  // create issue
-  const {
-    course,
-    user,
-    review,
-    reference,
-    difficulty,
-    quality,
-    sessionTaken,
-  } = json.details
+	// create issue
+	const {
+		course,
+		user,
+		review,
+		reference,
+		difficulty,
+		quality,
+		sessionTaken,
+	} = json.details
 
-  const title = `New review for ${course} by ${user}`
-  let body = `> ${review}\n>\n`
+	const title = `New review for ${course} by ${user}`
+	let body = `> ${review}\n>\n`
 
-  const parsedDifficulty = parseFloat(difficulty)
-  let clampedDifficulty = 0
-  if (!isNaN(parsedDifficulty)) {
-    clampedDifficulty = Math.min(Math.max(1, parsedDifficulty), 5)
-    body += `> Difficulty: ${clampedDifficulty}/5\n`
-  }
+	const parsedDifficulty = parseFloat(difficulty)
+	let clampedDifficulty = 0
+	if (!isNaN(parsedDifficulty)) {
+		clampedDifficulty = Math.min(Math.max(1, parsedDifficulty), 5)
+		body += `> Difficulty: ${clampedDifficulty}/5\n`
+	}
 
-  const parsedQuality = parseFloat(quality)
-  let clampedQuality = 0
-  if (!isNaN(parsedQuality)) {
-    clampedQuality = Math.min(Math.max(1, parsedQuality), 5)
-    body += `> Quality: ${clampedQuality}/5\n`
-  }
+	const parsedQuality = parseFloat(quality)
+	let clampedQuality = 0
+	if (!isNaN(parsedQuality)) {
+		clampedQuality = Math.min(Math.max(1, parsedQuality), 5)
+		body += `> Quality: ${clampedQuality}/5\n`
+	}
 
-  body += `> <cite><a href="${reference}">${user}</a>, ${new Date()
-    .toDateString()
-    .split(' ')
-    .slice(1)
-    .join(' ')}, course taken during ${sessionTaken}</cite>`
+	body += `> <cite><a href="${reference}">${user}</a>, ${new Date()
+		.toDateString()
+		.split(' ')
+		.slice(1)
+		.join(' ')}, course taken during ${sessionTaken}</cite>`
 
-  let yaml = `- author: ${user}\n  authorLink: ${reference}\n  date: ${new Date().getUTCFullYear()}-${(
-    new Date().getUTCMonth() + 1
-  )
-    .toString()
-    .padStart(2, '0')}-${new Date()
-    .getUTCDate()
-    .toString()
-    .padStart(2, '0')}\n  review: |\n    ${review.replaceAll('\n', '\n    ')}`
+	let yaml = `- author: ${user}\n  authorLink: ${reference}\n  date: ${new Date().getUTCFullYear()}-${(
+		new Date().getUTCMonth() + 1
+	)
+		.toString()
+		.padStart(2, '0')}-${new Date()
+		.getUTCDate()
+		.toString()
+		.padStart(2, '0')}\n  review: |\n    ${review.replaceAll('\n', '\n    ')}`
 
-  if (clampedDifficulty) {
-    yaml += `\n  difficulty: ${clampedDifficulty}`
-  }
-  if (clampedQuality) {
-    yaml += `\n  quality: ${clampedQuality}`
-  }
+	if (clampedDifficulty) {
+		yaml += `\n  difficulty: ${clampedDifficulty}`
+	}
+	if (clampedQuality) {
+		yaml += `\n  quality: ${clampedQuality}`
+	}
 
-  yaml += `\n  sessionTaken: ${sessionTaken}`
+	yaml += `\n  sessionTaken: ${sessionTaken}`
 
-  body += `\n<details><summary>View YAML to copy</summary>\n<pre>\n${yaml}\n<\pre>\n</details>`
+	body += `\n<details><summary>View YAML to copy</summary>\n<pre>\n${yaml}\n<\pre>\n</details>`
 
-  const response = await octokit.rest.issues.create({
-    owner: OWNER,
-    repo: REPO,
-    title: title,
-    body: body,
-    labels: LABELS.split(','),
-  })
+	const response = await octokit.rest.issues.create({
+		owner: OWNER,
+		repo: REPO,
+		title: title,
+		body: body,
+		labels: LABELS.split(','),
+	})
 
-  return new Response(JSON.stringify({ url: response.data.html_url }), {
-    headers: {
-      status: 201,
-      statusText: 'Created',
-      'Content-Type': 'application/json',
-      ...corsHeaders,
-    },
-  })
+	return new Response(JSON.stringify({ url: response.data.html_url }), {
+		headers: {
+			status: 201,
+			statusText: 'Created',
+			'Content-Type': 'application/json',
+			...corsHeaders,
+		},
+	})
 }
 
 /**
@@ -165,19 +165,19 @@ async function handleRequest(request) {
  * @param {string} token
  */
 async function verifyToken(token) {
-  try {
-    // add URL params
-    const url = new URL('https://www.google.com/recaptcha/api/siteverify')
-    url.searchParams.append('secret', RECAPTCHA_SECRET_KEY)
-    url.searchParams.append('response', token)
+	try {
+		// add URL params
+		const url = new URL('https://www.google.com/recaptcha/api/siteverify')
+		url.searchParams.append('secret', RECAPTCHA_SECRET_KEY)
+		url.searchParams.append('response', token)
 
-    // return verification result
-    const response = await fetch(url, {
-      method: 'POST',
-    })
-    const json = await response.json()
-    return { success: json.success, errors: json['error-codes'] }
-  } catch (e) {
-    return { success: false, errors: ['JSON parse failure'] }
-  }
+		// return verification result
+		const response = await fetch(url, {
+			method: 'POST',
+		})
+		const json = await response.json()
+		return { success: json.success, errors: json['error-codes'] }
+	} catch (e) {
+		return { success: false, errors: ['JSON parse failure'] }
+	}
 }
